aac572d74cce86b60461016c240efc206fdf9c12
Paulo Andrade <pauloeduardopandrade@gmail.com>
2022-09-04 11:55:47 -0300

    change definitions for feast 0.24

--- core/feature_definitions/breast_cancer.py
+++ core/feature_definitions/breast_cancer.py
@@ -1,16 +1,16 @@
 # Importing dependencies
-from google.protobuf.duration_pb2 import Duration
+from datetime import timedelta
 from feast.infra.offline_stores.contrib.postgres_offline_store.postgres_source import (
     PostgreSQLSource,
 )
-from feast import Entity, Feature, FeatureView, FeatureStore, ValueType
+from feast import Entity, Field, FeatureView, FeatureStore, FeatureService
+from feast.types import Float32, Int32
 
 store = FeatureStore(repo_path="./")
 
 # Declaring an entity for the dataset
 patient = Entity(
     name="patient_id", 
-    value_type=ValueType.INT64, 
     description="The ID of the patient")
 
 # Declaring the source of the first set of features
@@ -18,22 +18,22 @@ f_source1 = PostgreSQLSource(
     name="data_df1",
     query="SELECT * FROM feast.data_df1",
     timestamp_field="event_timestamp",
-    created_timestamp_column="event_timestamp",
+    created_timestamp_column="created_timestamp",
 )
 
 # Defining the first set of features
 df1_fv = FeatureView(
     name="df1_feature_view",
-    ttl=Duration(seconds=86400 * 3),
-    entities=["patient_id"],
-    features=[
-        Feature(name="mean radius", dtype=ValueType.FLOAT),
-        Feature(name="mean texture", dtype=ValueType.FLOAT),
-        Feature(name="mean perimeter", dtype=ValueType.FLOAT),
-        Feature(name="mean area", dtype=ValueType.FLOAT),
-        Feature(name="mean smoothness", dtype=ValueType.FLOAT)
+    ttl=timedelta(seconds=86400 * 3),
+    entities=[patient],
+    schema=[
+        Field(name="mean radius", dtype=Float32),
+        Field(name="mean texture", dtype=Float32),
+        Field(name="mean perimeter", dtype=Float32),
+        Field(name="mean area", dtype=Float32),
+        Field(name="mean smoothness", dtype=Float32)
         ],    
-    batch_source=f_source1
+    source=f_source1
 )
 
 # Declaring the source of the second set of features
@@ -41,22 +41,22 @@ f_source2 = PostgreSQLSource(
     name="data_df2",
     query="SELECT * FROM feast.data_df2",
     timestamp_field="event_timestamp",
-    created_timestamp_column="event_timestamp",
+    created_timestamp_column="created_timestamp",
 )
 
 # Defining the second set of features
 df2_fv = FeatureView(
     name="df2_feature_view",
-    ttl=Duration(seconds=86400 * 3),
-    entities=["patient_id"],
-    features=[
-        Feature(name="mean compactness", dtype=ValueType.FLOAT),
-        Feature(name="mean concavity", dtype=ValueType.FLOAT),
-        Feature(name="mean concave points", dtype=ValueType.FLOAT),
-        Feature(name="mean symmetry", dtype=ValueType.FLOAT),
-        Feature(name="mean fractal dimension", dtype=ValueType.FLOAT)
+    ttl=timedelta(seconds=86400 * 3),
+    entities=[patient],
+    schema=[
+        Field(name="mean compactness", dtype=Float32),
+        Field(name="mean concavity", dtype=Float32),
+        Field(name="mean concave points", dtype=Float32),
+        Field(name="mean symmetry", dtype=Float32),
+        Field(name="mean fractal dimension", dtype=Float32)
         ],    
-    batch_source=f_source2
+    source=f_source2
 )
 
 # Declaring the source of the third set of features
@@ -64,24 +64,24 @@ f_source3 = PostgreSQLSource(
     name="data_df3",
     query="SELECT * FROM feast.data_df3",
     timestamp_field="event_timestamp",
-    created_timestamp_column="event_timestamp",
+    created_timestamp_column="created_timestamp",
 )
 
 # Defining the third set of features
 df3_fv = FeatureView(
     name="df3_feature_view",
-    ttl=Duration(seconds=86400 * 3),
-    entities=["patient_id"],
-    features=[
-        Feature(name="radius error", dtype=ValueType.FLOAT),
-        Feature(name="texture error", dtype=ValueType.FLOAT),
-        Feature(name="perimeter error", dtype=ValueType.FLOAT),
-        Feature(name="area error", dtype=ValueType.FLOAT),
-        Feature(name="smoothness error", dtype=ValueType.FLOAT),
-        Feature(name="compactness error", dtype=ValueType.FLOAT),
-        Feature(name="concavity error", dtype=ValueType.FLOAT)
+    ttl=timedelta(seconds=86400 * 3),
+    entities=[patient],
+    schema=[
+        Field(name="radius error", dtype=Float32),
+        Field(name="texture error", dtype=Float32),
+        Field(name="perimeter error", dtype=Float32),
+        Field(name="area error", dtype=Float32),
+        Field(name="smoothness error", dtype=Float32),
+        Field(name="compactness error", dtype=Float32),
+        Field(name="concavity error", dtype=Float32)
         ],    
-    batch_source=f_source3
+    source=f_source3
 )
 
 # Declaring the source of the fourth set of features
@@ -89,30 +89,30 @@ f_source4 = PostgreSQLSource(
     name="data_df4",
     query="SELECT * FROM feast.data_df4",
     timestamp_field="event_timestamp",
-    created_timestamp_column="event_timestamp",
+    created_timestamp_column="created_timestamp",
 )
 
 # Defining the fourth set of features
 df4_fv = FeatureView(
     name="df4_feature_view",
-    ttl=Duration(seconds=86400 * 3),
-    entities=["patient_id"],
-    features=[
-        Feature(name="concave points error", dtype=ValueType.FLOAT),
-        Feature(name="symmetry error", dtype=ValueType.FLOAT),
-        Feature(name="fractal dimension error", dtype=ValueType.FLOAT),
-        Feature(name="worst radius", dtype=ValueType.FLOAT),
-        Feature(name="worst texture", dtype=ValueType.FLOAT),
-        Feature(name="worst perimeter", dtype=ValueType.FLOAT),
-        Feature(name="worst area", dtype=ValueType.FLOAT),
-        Feature(name="worst smoothness", dtype=ValueType.FLOAT),
-        Feature(name="worst compactness", dtype=ValueType.FLOAT),
-        Feature(name="worst concavity", dtype=ValueType.FLOAT),
-        Feature(name="worst concave points", dtype=ValueType.FLOAT),
-        Feature(name="worst symmetry", dtype=ValueType.FLOAT),
-        Feature(name="worst fractal dimension", dtype=ValueType.FLOAT),        
+    ttl=timedelta(seconds=86400 * 3),
+    entities=[patient],
+    schema=[
+        Field(name="concave points error", dtype=Float32),
+        Field(name="symmetry error", dtype=Float32),
+        Field(name="fractal dimension error", dtype=Float32),
+        Field(name="worst radius", dtype=Float32),
+        Field(name="worst texture", dtype=Float32),
+        Field(name="worst perimeter", dtype=Float32),
+        Field(name="worst area", dtype=Float32),
+        Field(name="worst smoothness", dtype=Float32),
+        Field(name="worst compactness", dtype=Float32),
+        Field(name="worst concavity", dtype=Float32),
+        Field(name="worst concave points", dtype=Float32),
+        Field(name="worst symmetry", dtype=Float32),
+        Field(name="worst fractal dimension", dtype=Float32),        
         ],    
-    batch_source=f_source4
+    source=f_source4
 )
 
 # Declaring the source of the targets
@@ -120,16 +120,26 @@ target_source = PostgreSQLSource(
     name="target_df",
     query="SELECT * FROM feast.target_df",
     timestamp_field="event_timestamp",
-    created_timestamp_column="event_timestamp",
+    created_timestamp_column="created_timestamp",
 )
 
 # Defining the targets
 target_fv = FeatureView(
     name="target_feature_view",
-    entities=["patient_id"],
-    ttl=Duration(seconds=86400 * 3),
-    features=[
-        Feature(name="target", dtype=ValueType.INT32)        
+    entities=[patient],
+    ttl=timedelta(seconds=86400 * 3),
+    schema=[
+        Field(name="target", dtype=Int32)        
         ],    
-    batch_source=target_source
+    source=target_source
+)
+
+df_fs = FeatureService(
+    name="df_feature_service",
+    features=[
+        df1_fv,
+        df2_fv,
+        df3_fv,
+        df4_fv
+    ],
 )
